bc30635817d59f759dd925237d1089632e8008b2
Jani Nikula <jani.nikula@intel.com>
2019-09-24 15:58:57 +0300

    drm/print: move drm_debug variable to drm_print.[ch]
    Move drm_debug variable declaration and definition to where they are
    relevant and needed. No functional changes.
    
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Eric Engestrom <eric@engestrom.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/71a566c68883b6e6c61414cd9f7c36c84015edb1.1569329774.git.jani.nikula@intel.com

--- sys/dev/drm/core/include/drm/drm_drv.h
+++ sys/dev/drm/core/include/drm/drm_drv.h
@@ -786,8 +786,6 @@ struct drm_driver {
 	int dev_priv_size;
 };
 
-extern unsigned int drm_debug;
-
 int drm_dev_init(struct drm_device *dev,
 		 struct drm_driver *driver,
 		 struct device *parent);
