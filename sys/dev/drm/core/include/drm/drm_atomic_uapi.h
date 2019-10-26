1656e065fe64d2af3be536cf8075afb2d0402723
Emmanuel Vadot <manu@freebsd.org>
2019-10-26 20:39:45 +0200

    More sync with linux 5.1

--- sys/dev/drm/core/include/drm/drm_atomic_uapi.h
+++ sys/dev/drm/core/include/drm/drm_atomic_uapi.h
@@ -29,6 +29,7 @@
 #ifndef DRM_ATOMIC_UAPI_H_
 #define DRM_ATOMIC_UAPI_H_
 
+#include <drm/drmP.h>
 struct drm_crtc_state;
 struct drm_display_mode;
 struct drm_property_blob;
@@ -38,20 +39,20 @@ struct drm_connector_state;
 struct dma_fence;
 struct drm_framebuffer;
 
-int 
+int __must_check
 drm_atomic_set_mode_for_crtc(struct drm_crtc_state *state,
 			     const struct drm_display_mode *mode);
-int
+int __must_check
 drm_atomic_set_mode_prop_for_crtc(struct drm_crtc_state *state,
 				  struct drm_property_blob *blob);
-int
+int __must_check
 drm_atomic_set_crtc_for_plane(struct drm_plane_state *plane_state,
 			      struct drm_crtc *crtc);
 void drm_atomic_set_fb_for_plane(struct drm_plane_state *plane_state,
 				 struct drm_framebuffer *fb);
 void drm_atomic_set_fence_for_plane(struct drm_plane_state *plane_state,
 				    struct dma_fence *fence);
-int
+int __must_check
 drm_atomic_set_crtc_for_connector(struct drm_connector_state *conn_state,
 				  struct drm_crtc *crtc);
 
