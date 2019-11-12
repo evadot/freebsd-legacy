79f89f51965f42fb2dca76f3e4de8791ff83cb23
Daniel Vetter <daniel.vetter@ffwll.ch>
2019-11-12 18:50:47 +0100

    drm/fb-helper: unexport drm_fb_helper_generic_probe
    Not sure we don't yet have this as a patch somewhere ...
    
    Motivation is that the automatic lifetime management of the generic fbdev
    code is quite tricky, and it'll get even more tricky. Allowing drivers
    to just use the fb_probe looks like a recipe for disaster.
    
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Noralf Trønnes <noralf@tronnes.org>
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Noralf Trønnes <noralf@tronnes.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191112175048.1581-1-daniel.vetter@ffwll.ch

--- sys/dev/drm/core/include/drm/drm_fb_helper.h
+++ sys/dev/drm/core/include/drm/drm_fb_helper.h
@@ -321,8 +321,6 @@ void drm_fb_helper_fbdev_teardown(struct drm_device *dev);
 void drm_fb_helper_lastclose(struct drm_device *dev);
 void drm_fb_helper_output_poll_changed(struct drm_device *dev);
 
-int drm_fb_helper_generic_probe(struct drm_fb_helper *fb_helper,
-				struct drm_fb_helper_surface_size *sizes);
 int drm_fbdev_generic_setup(struct drm_device *dev, unsigned int preferred_bpp);
 #else
 static inline void drm_fb_helper_prepare(struct drm_device *dev,
@@ -552,13 +550,6 @@ static inline void drm_fb_helper_lastclose(struct drm_device *dev)
 
 static inline void drm_fb_helper_output_poll_changed(struct drm_device *dev)
 {
-}
-
-static inline int
-drm_fb_helper_generic_probe(struct drm_fb_helper *fb_helper,
-			    struct drm_fb_helper_surface_size *sizes)
-{
-	return 0;
 }
 
 static inline int
