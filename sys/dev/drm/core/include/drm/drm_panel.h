4a989c28b9f0b7d80b9a3243700ee62e2014ac00
Emmanuel Vadot <manu@FreeBSD.org>
2020-02-19 18:28:12 +0100

    drmkpi: Add backlight.h
    Just here to please the compilation.
    IS_ENABLED doesn't work so switch to #if 0
    
    Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

--- sys/dev/drm/core/include/drm/drm_panel.h
+++ sys/dev/drm/core/include/drm/drm_panel.h
@@ -211,7 +211,7 @@ static inline struct drm_panel *of_drm_find_panel(const struct device_node *np)
 }
 #endif
 
-#if IS_ENABLED(CONFIG_BACKLIGHT_CLASS_DEVICE)
+#if 0 // IS_ENABLED(CONFIG_BACKLIGHT_CLASS_DEVICE)
 int drm_panel_of_backlight(struct drm_panel *panel);
 #else
 static inline int drm_panel_of_backlight(struct drm_panel *panel)
