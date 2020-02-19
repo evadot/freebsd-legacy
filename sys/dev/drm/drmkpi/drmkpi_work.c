5277bf593b62f4061b1025dee00c531a348c947a
Emmanuel Vadot <manu@FreeBSD.org>
2020-02-19 20:06:35 +0100

    drmkpi: Prefix drmkpi_work.c function with drmkpi_ to avoid clash with linuxkpi
    Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

--- sys/dev/drm/drmkpi/drmkpi_work.c
+++ sys/dev/drm/drmkpi/drmkpi_work.c
@@ -49,8 +49,8 @@ enum {
 /*
  * Define global workqueues
  */
-static struct workqueue_struct *linux_system_short_wq;
-static struct workqueue_struct *linux_system_long_wq;
+static struct workqueue_struct *drmkpi_system_short_wq;
+static struct workqueue_struct *drmkpi_system_long_wq;
 
 struct workqueue_struct *system_wq;
 struct workqueue_struct *system_long_wq;
@@ -58,16 +58,16 @@ struct workqueue_struct *system_unbound_wq;
 struct workqueue_struct *system_highpri_wq;
 struct workqueue_struct *system_power_efficient_wq;
 
-static int linux_default_wq_cpus = 4;
+static int drmkpi_default_wq_cpus = 4;
 
-static void linux_delayed_work_timer_fn(void *);
+static void drmkpi_delayed_work_timer_fn(void *);
 
 /*
  * This function atomically updates the work state and returns the
  * previous state at the time of update.
  */
 static uint8_t
-linux_update_state(atomic_t *v, const uint8_t *pstate)
+drmkpi_update_state(atomic_t *v, const uint8_t *pstate)
 {
 	int c, old;
 
@@ -82,13 +82,13 @@ linux_update_state(atomic_t *v, const uint8_t *pstate)
 /*
  * A LinuxKPI task is allowed to free itself inside the callback function
  * and cannot safely be referred after the callback function has
- * completed. This function gives the linux_work_fn() function a hint,
+ * completed. This function gives the drmkpi_work_fn() function a hint,
  * that the task is not going away and can have its state checked
  * again. Without this extra hint LinuxKPI tasks cannot be serialized
  * accross multiple worker threads.
  */
 static bool
-linux_work_exec_unblock(struct work_struct *work)
+drmkpi_work_exec_unblock(struct work_struct *work)
 {
 	struct workqueue_struct *wq;
 	struct work_exec *exec;
@@ -112,7 +112,7 @@ done:
 }
 
 static void
-linux_delayed_work_enqueue(struct delayed_work *dwork)
+drmkpi_delayed_work_enqueue(struct delayed_work *dwork)
 {
 	struct taskqueue *tq;
 
@@ -126,7 +126,7 @@ linux_delayed_work_enqueue(struct delayed_work *dwork)
  * [re-]queued. Else the work is already pending for completion.
  */
 bool
-linux_queue_work_on(int cpu __unused, struct workqueue_struct *wq,
+drmkpi_queue_work_on(int cpu __unused, struct workqueue_struct *wq,
     struct work_struct *work)
 {
 	static const uint8_t states[WORK_ST_MAX] __aligned(8) = {
@@ -140,10 +140,10 @@ linux_queue_work_on(int cpu __unused, struct workqueue_struct *wq,
 	if (atomic_read(&wq->draining) != 0)
 		return (!work_pending(work));
 
-	switch (linux_update_state(&work->state, states)) {
+	switch (drmkpi_update_state(&work->state, states)) {
 	case WORK_ST_EXEC:
 	case WORK_ST_CANCEL:
-		if (linux_work_exec_unblock(work) != 0)
+		if (drmkpi_work_exec_unblock(work) != 0)
 			return (1);
 		/* FALLTHROUGH */
 	case WORK_ST_IDLE:
@@ -162,7 +162,7 @@ linux_queue_work_on(int cpu __unused, struct workqueue_struct *wq,
  * for completion.
  */
 bool
-linux_queue_delayed_work_on(int cpu, struct workqueue_struct *wq,
+drmkpi_queue_delayed_work_on(int cpu, struct workqueue_struct *wq,
     struct delayed_work *dwork, unsigned delay)
 {
 	static const uint8_t states[WORK_ST_MAX] __aligned(8) = {
@@ -176,10 +176,10 @@ linux_queue_delayed_work_on(int cpu, struct workqueue_struct *wq,
 	if (atomic_read(&wq->draining) != 0)
 		return (!work_pending(&dwork->work));
 
-	switch (linux_update_state(&dwork->work.state, states)) {
+	switch (drmkpi_update_state(&dwork->work.state, states)) {
 	case WORK_ST_EXEC:
 	case WORK_ST_CANCEL:
-		if (delay == 0 && linux_work_exec_unblock(&dwork->work) != 0) {
+		if (delay == 0 && drmkpi_work_exec_unblock(&dwork->work) != 0) {
 			dwork->timer.expires = jiffies;
 			return (1);
 		}
@@ -189,16 +189,16 @@ linux_queue_delayed_work_on(int cpu, struct workqueue_struct *wq,
 		dwork->timer.expires = jiffies + delay;
 
 		if (delay == 0) {
-			linux_delayed_work_enqueue(dwork);
+			drmkpi_delayed_work_enqueue(dwork);
 		} else if (unlikely(cpu != WORK_CPU_UNBOUND)) {
 			mtx_lock(&dwork->timer.mtx);
 			callout_reset_on(&dwork->timer.callout, delay,
-			    &linux_delayed_work_timer_fn, dwork, cpu);
+			    &drmkpi_delayed_work_timer_fn, dwork, cpu);
 			mtx_unlock(&dwork->timer.mtx);
 		} else {
 			mtx_lock(&dwork->timer.mtx);
 			callout_reset(&dwork->timer.callout, delay,
-			    &linux_delayed_work_timer_fn, dwork);
+			    &drmkpi_delayed_work_timer_fn, dwork);
 			mtx_unlock(&dwork->timer.mtx);
 		}
 		return (1);
@@ -208,7 +208,7 @@ linux_queue_delayed_work_on(int cpu, struct workqueue_struct *wq,
 }
 
 void
-linux_work_fn(void *context, int pending)
+drmkpi_work_fn(void *context, int pending)
 {
 	static const uint8_t states[WORK_ST_MAX] __aligned(8) = {
 		[WORK_ST_IDLE] = WORK_ST_IDLE,		/* NOP */
@@ -235,7 +235,7 @@ linux_work_fn(void *context, int pending)
 	WQ_EXEC_LOCK(wq);
 	TAILQ_INSERT_TAIL(&wq->exec_head, &exec, entry);
 	while (1) {
-		switch (linux_update_state(&work->state, states)) {
+		switch (drmkpi_update_state(&work->state, states)) {
 		case WORK_ST_TIMER:
 		case WORK_ST_TASK:
 		case WORK_ST_CANCEL:
@@ -269,7 +269,7 @@ done:
 }
 
 void
-linux_delayed_work_fn(void *context, int pending)
+drmkpi_delayed_work_fn(void *context, int pending)
 {
 	struct delayed_work *dwork = context;
 
@@ -282,11 +282,11 @@ linux_delayed_work_fn(void *context, int pending)
 	 */
 	callout_drain(&dwork->timer.callout);
 
-	linux_work_fn(&dwork->work, pending);
+	drmkpi_work_fn(&dwork->work, pending);
 }
 
 static void
-linux_delayed_work_timer_fn(void *arg)
+drmkpi_delayed_work_timer_fn(void *arg)
 {
 	static const uint8_t states[WORK_ST_MAX] __aligned(8) = {
 		[WORK_ST_IDLE] = WORK_ST_IDLE,		/* NOP */
@@ -297,10 +297,10 @@ linux_delayed_work_timer_fn(void *arg)
 	};
 	struct delayed_work *dwork = arg;
 
-	switch (linux_update_state(&dwork->work.state, states)) {
+	switch (drmkpi_update_state(&dwork->work.state, states)) {
 	case WORK_ST_TIMER:
 	case WORK_ST_CANCEL:
-		linux_delayed_work_enqueue(dwork);
+		drmkpi_delayed_work_enqueue(dwork);
 		break;
 	default:
 		break;
@@ -313,7 +313,7 @@ linux_delayed_work_timer_fn(void *arg)
  * cancelled. Else the work was already cancelled.
  */
 bool
-linux_cancel_work_sync(struct work_struct *work)
+drmkpi_cancel_work_sync(struct work_struct *work)
 {
 	static const uint8_t states[WORK_ST_MAX] __aligned(8) = {
 		[WORK_ST_IDLE] = WORK_ST_IDLE,		/* NOP */
@@ -325,9 +325,9 @@ linux_cancel_work_sync(struct work_struct *work)
 	struct taskqueue *tq;
 
 	WITNESS_WARN(WARN_GIANTOK | WARN_SLEEPOK, NULL,
-	    "linux_cancel_work_sync() might sleep");
+	    "drmkpi_cancel_work_sync() might sleep");
 
-	switch (linux_update_state(&work->state, states)) {
+	switch (drmkpi_update_state(&work->state, states)) {
 	case WORK_ST_IDLE:
 	case WORK_ST_TIMER:
 		return (0);
@@ -351,7 +351,7 @@ linux_cancel_work_sync(struct work_struct *work)
  * timeout was not started or has already been called.
  */
 static inline bool
-linux_cancel_timer(struct delayed_work *dwork, bool drain)
+drmkpi_cancel_timer(struct delayed_work *dwork, bool drain)
 {
 	bool cancelled;
 
@@ -372,7 +372,7 @@ linux_cancel_timer(struct delayed_work *dwork, bool drain)
  * cancelled.
  */
 bool
-linux_cancel_delayed_work(struct delayed_work *dwork)
+drmkpi_cancel_delayed_work(struct delayed_work *dwork)
 {
 	static const uint8_t states[WORK_ST_MAX] __aligned(8) = {
 		[WORK_ST_IDLE] = WORK_ST_IDLE,		/* NOP */
@@ -383,10 +383,10 @@ linux_cancel_delayed_work(struct delayed_work *dwork)
 	};
 	struct taskqueue *tq;
 
-	switch (linux_update_state(&dwork->work.state, states)) {
+	switch (drmkpi_update_state(&dwork->work.state, states)) {
 	case WORK_ST_TIMER:
 	case WORK_ST_CANCEL:
-		if (linux_cancel_timer(dwork, 0)) {
+		if (drmkpi_cancel_timer(dwork, 0)) {
 			atomic_cmpxchg(&dwork->work.state,
 			    WORK_ST_CANCEL, WORK_ST_IDLE);
 			return (1);
@@ -411,7 +411,7 @@ linux_cancel_delayed_work(struct delayed_work *dwork)
  * cancelled. Else the work was already cancelled.
  */
 bool
-linux_cancel_delayed_work_sync(struct delayed_work *dwork)
+drmkpi_cancel_delayed_work_sync(struct delayed_work *dwork)
 {
 	static const uint8_t states[WORK_ST_MAX] __aligned(8) = {
 		[WORK_ST_IDLE] = WORK_ST_IDLE,		/* NOP */
@@ -423,9 +423,9 @@ linux_cancel_delayed_work_sync(struct delayed_work *dwork)
 	struct taskqueue *tq;
 
 	WITNESS_WARN(WARN_GIANTOK | WARN_SLEEPOK, NULL,
-	    "linux_cancel_delayed_work_sync() might sleep");
+	    "drmkpi_cancel_delayed_work_sync() might sleep");
 
-	switch (linux_update_state(&dwork->work.state, states)) {
+	switch (drmkpi_update_state(&dwork->work.state, states)) {
 	case WORK_ST_IDLE:
 		return (0);
 	case WORK_ST_EXEC:
@@ -435,7 +435,7 @@ linux_cancel_delayed_work_sync(struct delayed_work *dwork)
 		return (0);
 	case WORK_ST_TIMER:
 	case WORK_ST_CANCEL:
-		if (linux_cancel_timer(dwork, 1)) {
+		if (drmkpi_cancel_timer(dwork, 1)) {
 			/*
 			 * Make sure taskqueue is also drained before
 			 * returning:
@@ -459,13 +459,13 @@ linux_cancel_delayed_work_sync(struct delayed_work *dwork)
  * waited for. Else the work was not waited for.
  */
 bool
-linux_flush_work(struct work_struct *work)
+drmkpi_flush_work(struct work_struct *work)
 {
 	struct taskqueue *tq;
 	int retval;
 
 	WITNESS_WARN(WARN_GIANTOK | WARN_SLEEPOK, NULL,
-	    "linux_flush_work() might sleep");
+	    "drmkpi_flush_work() might sleep");
 
 	switch (atomic_read(&work->state)) {
 	case WORK_ST_IDLE:
@@ -484,20 +484,20 @@ linux_flush_work(struct work_struct *work)
  * for. Else the work was not waited for.
  */
 bool
-linux_flush_delayed_work(struct delayed_work *dwork)
+drmkpi_flush_delayed_work(struct delayed_work *dwork)
 {
 	struct taskqueue *tq;
 	int retval;
 
 	WITNESS_WARN(WARN_GIANTOK | WARN_SLEEPOK, NULL,
-	    "linux_flush_delayed_work() might sleep");
+	    "drmkpi_flush_delayed_work() might sleep");
 
 	switch (atomic_read(&dwork->work.state)) {
 	case WORK_ST_IDLE:
 		return (0);
 	case WORK_ST_TIMER:
-		if (linux_cancel_timer(dwork, 1))
-			linux_delayed_work_enqueue(dwork);
+		if (drmkpi_cancel_timer(dwork, 1))
+			drmkpi_delayed_work_enqueue(dwork);
 		/* FALLTHROUGH */
 	default:
 		tq = dwork->work.work_queue->taskqueue;
@@ -512,7 +512,7 @@ linux_flush_delayed_work(struct delayed_work *dwork)
  * yet executing:
  */
 bool
-linux_work_pending(struct work_struct *work)
+drmkpi_work_pending(struct work_struct *work)
 {
 	switch (atomic_read(&work->state)) {
 	case WORK_ST_TIMER:
@@ -528,7 +528,7 @@ linux_work_pending(struct work_struct *work)
  * This function returns true if the given work is busy.
  */
 bool
-linux_work_busy(struct work_struct *work)
+drmkpi_work_busy(struct work_struct *work)
 {
 	struct taskqueue *tq;
 
@@ -544,7 +544,7 @@ linux_work_busy(struct work_struct *work)
 }
 
 struct workqueue_struct *
-linux_create_workqueue_common(const char *name, int cpus)
+drmkpi_create_workqueue_common(const char *name, int cpus)
 {
 	struct workqueue_struct *wq;
 
@@ -552,7 +552,7 @@ linux_create_workqueue_common(const char *name, int cpus)
 	 * If zero CPUs are specified use the default number of CPUs:
 	 */
 	if (cpus == 0)
-		cpus = linux_default_wq_cpus;
+		cpus = drmkpi_default_wq_cpus;
 
 	wq = kmalloc(sizeof(*wq), M_WAITOK | M_ZERO);
 	wq->taskqueue = taskqueue_create(name, M_WAITOK,
@@ -560,13 +560,13 @@ linux_create_workqueue_common(const char *name, int cpus)
 	atomic_set(&wq->draining, 0);
 	taskqueue_start_threads(&wq->taskqueue, cpus, PWAIT, "%s", name);
 	TAILQ_INIT(&wq->exec_head);
-	mtx_init(&wq->exec_mtx, "linux_wq_exec", NULL, MTX_DEF);
+	mtx_init(&wq->exec_mtx, "drmkpi_wq_exec", NULL, MTX_DEF);
 
 	return (wq);
 }
 
 void
-linux_destroy_workqueue(struct workqueue_struct *wq)
+drmkpi_destroy_workqueue(struct workqueue_struct *wq)
 {
 	atomic_inc(&wq->draining);
 	drain_workqueue(wq);
@@ -576,24 +576,24 @@ linux_destroy_workqueue(struct workqueue_struct *wq)
 }
 
 void
-linux_init_delayed_work(struct delayed_work *dwork, work_func_t func)
+drmkpi_init_delayed_work(struct delayed_work *dwork, work_func_t func)
 {
 	memset(dwork, 0, sizeof(*dwork));
 	dwork->work.func = func;
-	TASK_INIT(&dwork->work.work_task, 0, linux_delayed_work_fn, dwork);
+	TASK_INIT(&dwork->work.work_task, 0, drmkpi_delayed_work_fn, dwork);
 	mtx_init(&dwork->timer.mtx, spin_lock_name("lkpi-dwork"), NULL,
 	    MTX_DEF | MTX_NOWITNESS);
 	callout_init_mtx(&dwork->timer.callout, &dwork->timer.mtx, 0);
 }
 
 struct work_struct *
-linux_current_work(void)
+drmkpi_current_work(void)
 {
 	return (current->work);
 }
 
 static void
-linux_work_init(void *arg)
+drmkpi_work_init(void *arg)
 {
 	int max_wq_cpus = mp_ncpus + 1;
 
@@ -602,25 +602,25 @@ linux_work_init(void *arg)
 		max_wq_cpus = 4;
 
 	/* set default number of CPUs */
-	linux_default_wq_cpus = max_wq_cpus;
+	drmkpi_default_wq_cpus = max_wq_cpus;
 
-	linux_system_short_wq = alloc_workqueue("linuxkpi_short_wq", 0, max_wq_cpus);
-	linux_system_long_wq = alloc_workqueue("linuxkpi_long_wq", 0, max_wq_cpus);
+	drmkpi_system_short_wq = alloc_workqueue("linuxkpi_short_wq", 0, max_wq_cpus);
+	drmkpi_system_long_wq = alloc_workqueue("linuxkpi_long_wq", 0, max_wq_cpus);
 
 	/* populate the workqueue pointers */
-	system_long_wq = linux_system_long_wq;
-	system_wq = linux_system_short_wq;
-	system_power_efficient_wq = linux_system_short_wq;
-	system_unbound_wq = linux_system_short_wq;
-	system_highpri_wq = linux_system_short_wq;
+	system_long_wq = drmkpi_system_long_wq;
+	system_wq = drmkpi_system_short_wq;
+	system_power_efficient_wq = drmkpi_system_short_wq;
+	system_unbound_wq = drmkpi_system_short_wq;
+	system_highpri_wq = drmkpi_system_short_wq;
 }
-SYSINIT(linux_work_init, SI_SUB_TASKQ, SI_ORDER_THIRD, linux_work_init, NULL);
+SYSINIT(drmkpi_work_init, SI_SUB_TASKQ, SI_ORDER_THIRD, drmkpi_work_init, NULL);
 
 static void
-linux_work_uninit(void *arg)
+drmkpi_work_uninit(void *arg)
 {
-	destroy_workqueue(linux_system_short_wq);
-	destroy_workqueue(linux_system_long_wq);
+	destroy_workqueue(drmkpi_system_short_wq);
+	destroy_workqueue(drmkpi_system_long_wq);
 
 	/* clear workqueue pointers */
 	system_long_wq = NULL;
@@ -629,4 +629,4 @@ linux_work_uninit(void *arg)
 	system_unbound_wq = NULL;
 	system_highpri_wq = NULL;
 }
-SYSUNINIT(linux_work_uninit, SI_SUB_TASKQ, SI_ORDER_THIRD, linux_work_uninit, NULL);
+SYSUNINIT(drmkpi_work_uninit, SI_SUB_TASKQ, SI_ORDER_THIRD, drmkpi_work_uninit, NULL);
